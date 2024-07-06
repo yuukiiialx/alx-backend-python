9072243f6f0f5618856d5d6dd85f302aeaba21a2
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-06 05:46:57 +0300

    Update 9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" 9-element_length.py"""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """element_length"""
+    return [(i, len(i)) for i in lst]
