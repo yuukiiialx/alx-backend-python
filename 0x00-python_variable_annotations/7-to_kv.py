6b8bb6e0b32623b523cbd50553fbc8f68340725d
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-06 05:46:09 +0300

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" 7-to_kv.py """
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """to_kv"""
+
+    return (k, v**2)
