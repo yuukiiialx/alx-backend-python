512d5f5845ba76028ccd44148307914077c675f0
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-06 05:45:49 +0300

    Update 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" 5-sum_list.py """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """sum_list"""
+
+    return sum(input_list)
