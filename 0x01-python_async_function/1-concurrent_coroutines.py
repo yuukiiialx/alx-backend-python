358ea3aa36f6e4e29104cba445cab0f8d7f0f278
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-09 05:14:03 +0300

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,28 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+    1-concurrent_coroutines.py
+"""
+import asyncio
+
+from typing import List
+
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    wait_n
+    @n: times to call wait_random
+    @max_delay: max delay of wait_random
+    Return: list of all delays
+    """
+
+    delays: List[asyncio.Future[float]] = []
+    for _ in range(n):
+        delays.append(wait_random(max_delay))
+
+    result: List[float] = []
+    for delay in asyncio.as_completed(delays):
+        result.append(await delay)
+    return result
