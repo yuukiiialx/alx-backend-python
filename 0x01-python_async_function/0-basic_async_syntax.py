aa4a6e1aa6e72d252d3458cc37c086810cc07a6e
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-09 05:13:00 +0300

    Update 0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+    0-basic_async_syntax.py
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """wait_random"""
+    random_delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(random_delay)
+    return random_delay
