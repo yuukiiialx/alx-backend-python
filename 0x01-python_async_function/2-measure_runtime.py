840b6bc9611c7bfb541bd66b60d19eb0e2c323b6
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-09 05:13:17 +0300

    Update 2-measure_runtime.py

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,1 +1,24 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+    2-measure_runtime.py
+"""
+
+import asyncio
+import time
+
+wait_n = __import__("1-concurrent_coroutines").wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    measures the total execution time for wait_n(n, max_delay)
+    returns total_time / n
+    """
+    start: float = time.perf_counter()
+
+    asyncio.run(wait_n(n, max_delay))
+
+    end: float = time.perf_counter()
+
+    total_time: float = end - start
+    return total_time / n
