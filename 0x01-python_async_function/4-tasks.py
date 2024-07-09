28f43238d0025511727d7b5de59558d5c350b333
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-09 05:16:09 +0300

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,29 +1,27 @@
-#!/usr/bin/python3
 #!/usr/bin/env python3
-"""
-    4-tasks.py module
-"""
-import asyncio
+''' Description: Take the code from wait_n and alter it into a new function
+                 task_wait_n. The code is nearly identical to wait_n except
+                 task_wait_random is being called.
+    Arguments: n: int, max_delay: int = 10
+'''
 
 from typing import List
+import asyncio
+import random
+
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-task_wait_random = __import__("3-tasks").task_wait_random
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    '''Execute task_wait_random and returns sorted list of delay'''
+    spawn_ls = []
+    delay_ls = []
+    for i in range(n):
+        delayed_task = task_wait_random(max_delay)
+        delayed_task.add_done_callback(lambda x: delay_ls.append(x.result()))
+        spawn_ls.append(delayed_task)
 
+    for spawn in spawn_ls:
+        await spawn
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    task_wait_n
-    @n: times to call wait_random
-    @max_delay: max delay of wait_random
-    Return: list of all delays
-    """
-
-    delays: List[asyncio.Future[float]] = []
-    for _ in range(n):
-        delays.append(task_wait_random(max_delay))
-
-    result: List[float] = []
-    for delay in asyncio.as_completed(delays):
-        result.append(await delay)
-    return result
+    return delay_ls
