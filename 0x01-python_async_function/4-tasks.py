4ac50962bb83cf4d5888a7f5d70492a6b2c08a6d
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-09 05:13:47 +0300

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+    4-tasks.py module
+"""
+import asyncio
+
+from typing import List
+
+
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    task_wait_n
+    @n: times to call wait_random
+    @max_delay: max delay of wait_random
+    Return: list of all delays
+    """
+
+    delays: List[asyncio.Future[float]] = []
+    for _ in range(n):
+        delays.append(task_wait_random(max_delay))
+
+    result: List[float] = []
+    for delay in asyncio.as_completed(delays):
+        result.append(await delay)
+    return result
