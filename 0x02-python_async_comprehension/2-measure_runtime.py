6d67b22dfbbbe01eaf0c2b5aa63dda9951f763e4
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-12 22:12:48 +0300

    Update 2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,24 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+    2-measure_runtime.py
+"""
+
+
+import asyncio
+import time
+
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    """measure_runtime"""
+    start: float = time.perf_counter()
+
+    tasks = [async_comprehension() for i in range(4)]
+
+    await asyncio.gather(*tasks)
+
+    end: float = time.perf_counter()
+
+    return end - start
