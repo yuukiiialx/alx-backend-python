bf2fa1182ec406ea08a04ab0f9bc20dbd1222979
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-12 22:12:36 +0300

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+    1-async_comprehension
+"""
+
+from typing import List
+
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """async_comprehension"""
+    return [i async for i in async_generator()]
